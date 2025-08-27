1f693bed250dd756bce4856a4d636d4e206d303d
hawariyaw paulos <hawipaul308@gmail.com>
2025-08-27 21:44:48 +0300

    full-stack2

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const port = 4000;
+const express = require("express");
+const app = express;
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// creating a mongodb atlas database
+// Database connection with mongodb
+mongoose.connect('mongodb+srv://hawipaul:hawipaul@cluster0.t49bxrz.mongodb.net/e-commerce');
+// updated the password part to my database password, mongodb+srv://hawipaul:<db_password>@cluster0.t49bxrz.mongodb.net/
+// also added a path where we will store data of our application
+
+
+// API Endpoint Creation
+app.get('/', (req,res)=>{
+    res.send('Express app is Running');
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log('Server Running on port'+port);
+    }
+    else{
+        console.log('Error: '+error);
+    }
+})
