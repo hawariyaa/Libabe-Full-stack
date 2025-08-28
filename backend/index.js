a6eacd9efcdcb04b78e4b9671cb263500b04de1c
hawariyaw paulos <hawipaul308@gmail.com>
2025-08-28 23:27:52 +0300

    full-stack3

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const port = 4000;
 const express = require("express");
-const app = express;
+const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
-const path = require("cors");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
