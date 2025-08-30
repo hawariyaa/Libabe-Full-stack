e3e89928bb82a6f44bcf46157cfe67569abb405c
hawariyaw paulos <hawipaul308@gmail.com>
2025-08-30 23:24:50 +0300

    full-stack3

--- backend/index.js
+++ backend/index.js
@@ -29,3 +29,21 @@ app.listen(port,(error)=>{
         console.log('Error: '+error);
     }
 })
+
+const storage = multer.diskStorage({
+    destination:'./upload/images',
+    filename:(req,file,cb)=>{
+           return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage: storage});
+
+app.use('/images', express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
