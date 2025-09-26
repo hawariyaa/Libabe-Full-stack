3f4166c3a71566e1c7db24f22261f9be29ef8c94
hawariyaw paulos <hawipaul308@gmail.com>
2025-09-26 21:19:51 +0300

    full-stack10

--- Admin/src/components/Addproduct.jsx
+++ Admin/src/components/Addproduct.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
-
+import {useState} from 'react'
+import upload_area from '../assets/Admin_Assets/upload_area.svg'
+import '../css/addproduct.css'
 function Addproduct() {
+
+  const [image, setImage] = useState(false);
+  const ImageHandler = (e) =>{
+    setImage(e.target.files[0]);
+  }
   return (
-    <div>Addproduct</div>
+    
+      <div className="add-product">
+        <div className="addproduct-itemfield">
+          <p>Product Title</p>
+          <input type="text" name='name' placeholder='Title here' />
+        </div>
+        <div className="addproduct-price">
+          <div className="addproduct-itemfield">
+            <p>Price</p>
+            <input type="text" name='old_price' placeholder='old price' />
+          </div>
+          <div className="addproduct-itemfield">
+            <p>Offer Price</p>
+            <input type="text" name='new_price' placeholder='new price' />
+          </div>
+        </div>
+        <div className="addproduct-itemfield">
+          <p>Product Category</p>
+          <select name="category" className="add-product-selector">
+            <option value="women">women</option>
+            <option value="men">Men</option>
+            <option value="kid">Kid</option>
+          </select>
+        </div>
+        <div className="addproduct-itemfield">
+          <p>Upload Image of Product</p>
+          <label htmlFor="file-input">
+            <img src={image?URL.createObjectURL(image):upload_area} className='addproduct-tumbnail' alt="" />
+          </label>
+          <input onChange={ImageHandler} type="file" name='image' id='file-input' hidden />
+        </div>
+        <button className='product-btn'>ADD</button>
+      </div>
+    
   )
 }
 
