c5eee610c85b59189cd18e6e5a2c971d2d11ff5d
hawariyaw paulos <hawipaul308@gmail.com>
2025-09-30 19:07:03 +0300

    fullstack13

--- Admin/src/components/Listproduct.jsx
+++ Admin/src/components/Listproduct.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../css/listproduct.css'
+import { useEffect } from 'react';
+import removeicon from '../assets/Admin_Assets/cross_icon.png'
 
 function Listproduct() {
     
@@ -7,7 +9,21 @@ function Listproduct() {
   const fetchInfo = async ()=>{
     await fetch('http://localhost:4000/allproducts').then((res)=>res.json()).then((data)=>{setAllProducts(data)});
   }
+  useEffect(()=>{
+    fetchInfo();
+  }, []); //by adding one square bracket the function will only be executed once
 
+  const removeproduct = async(id) =>{
+       await fetch('http//:localhost:4000/removeproduct', {
+        method:'POST',
+        header:{
+          Accept:'application/json',
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify({id:id})
+       })
+       await fetchInfo();
+  }
 
   return (
   <div className="list-product">
@@ -22,6 +38,18 @@ function Listproduct() {
     </div>
     <div className="listproduct-allproducts">
       <hr />
+      {allproducts.map((product, index)=>{
+        return <><div key={index} className="list-product-main">
+          <img src={product.image} alt="" className="listprocut-product-icon" />
+          <p>{product.name}</p>
+          <p>${product.old_price}</p>
+          <p>${product.new_price}</p>
+          <p>{product.category}</p>
+          <img src={removeicon} onClick={()=>{removeproduct(product.id)}} alt="" className="list-product-removeicon" />
+        </div>
+        <hr />
+        </>
+      })}
     </div>
   </div>
   )
